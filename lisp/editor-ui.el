e2f4a50cd2510736ecca8b187a744b4d5c998327
jsperger <j.sperger@gmail.com>
2023-11-24 20:31:33 -0500

    Split themes into own file

--- lisp/editor-ui.el
+++ lisp/editor-ui.el
@@ -8,34 +8,6 @@
 
 ;;; Code:
 
-(defvar after-load-theme-hook nil
-  "Hook run after a color theme is loaded using `load-theme'.")
-
-(defun load-theme@run-hooks (&rest _)
-  "Run `after-load-theme-hook'."
-  (run-hooks 'after-load-theme-hook))
-(advice-add 'load-theme :after #'load-theme@run-hooks)
-
-(defun load-theme@theme-dont-propagate (&rest _)
-  "Discard all themes before loading new."
-  (mapc #'disable-theme custom-enabled-themes))
-(advice-add #'load-theme :before #'load-theme@theme-dont-propagate)
-
-(add-hook 'after-load-theme-hook
-          (defun bolder-faces ()
-            (set-face-attribute 'font-lock-function-name-face nil :weight 'semi-bold)
-            (set-face-attribute 'font-lock-keyword-face nil :weight 'semi-bold)))
-
-(add-hook 'after-load-theme-hook
-          (defun unscale-outlines ()
-            (dolist (outline-number (number-sequence 1 8))
-              (let ((outline (intern (format "outline-%d" outline-number))))
-                (set-face-attribute outline nil :height 1.0)))))
-
-(use-package doom-themes
-  :defer t)
-
-
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode)
   :config
