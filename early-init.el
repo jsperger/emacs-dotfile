065c7f223d6879fb1642b361d0eb894236bad766
John Sperger <j.sperger@gmail.com>
2024-10-24 21:42:05 -0400

    Reorder elpaca menu functions

--- early-init.el
+++ early-init.el
@@ -13,11 +13,11 @@
 ;; e.g. melpa over devel
 (setq elpaca-menu-functions '(elpaca-menu-org
                               elpaca-menu-melpa
-                              elpaca-menu-non-gnu-elpa
                               elpaca-menu-gnu-elpa
+                              elpaca-menu-non-gnu-elpa
 			      elpaca-menu-extensions
-                              elpaca-menu-non-gnu-devel-elpa
                               elpaca-menu-gnu-devel-elpa
+                              elpaca-menu-non-gnu-devel-elpa
    ))
 
 ;; Inhibit resizing frame
