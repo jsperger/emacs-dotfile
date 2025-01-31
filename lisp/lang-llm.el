f8d6f8b3a204de8910e15deec01f2b3b629da1ce
John Sperger <j.sperger@gmail.com>
2025-01-31 13:01:20 -0500

    Add gptel for testing. Remove OS-logic for ellama

--- lisp/lang-llm.el
+++ lisp/lang-llm.el
@@ -8,12 +8,20 @@
 
 ;;; Code:
 
-(if (eq system-type 'gnu/linux)
-    (use-package ellama
-      :config
-      (setopt ellama-language "English")))
 
+(use-package ellama
+  :config
+  (setopt ellama-language "English"))
 
+(use-package gptel
+  :config
+  ;; Llama.cpp offers an OpenAI compatible API
+(gptel-make-openai "llama-cpp"          ;Any name
+  :stream t                             ;Stream responses
+  :protocol "http"
+  :host "localhost:8000"                ;Llama.cpp server location
+  :models '(test))                    ;Any names, doesn't matter for Llama)
+)
 
 (provide 'lang-llm)
 
