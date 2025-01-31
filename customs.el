835ee45a9ffd186a3e87a5a6c38860b02960e5b7
John Sperger <j.sperger@gmail.com>
2025-01-31 12:57:42 -0500

    Mac: make fullscreen at startup using toggle-frame-fullscreen

--- customs.el
+++ customs.el
@@ -157,5 +157,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(when (eq system-type 'darwin)
+  (toggle-frame-fullscreen)
+)
 
 ;;; customs.el ends here
