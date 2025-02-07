73a9c06ae8f0db36e90be4fb95fe7dac58ba50a0
John Sperger <j.sperger@gmail.com>
2025-02-07 17:17:11 -0500

    Disable LLM packages until I have time to setup/debug.

--- lisp/lang-llm.el
+++ lisp/lang-llm.el
@@ -9,17 +9,26 @@
 ;;; Code:
 
 
+
 (use-package ellama
+  :disabled
+  :init
+  (require 'llm-openai-compatible)
+   (setopt ellama-provider
+  	       (make-llm-openai-comptaible
+  :url "http://127.0.0.1:1234"))
   :config
-  (setopt ellama-language "English"))
+  (setopt ellama-language "English")
+  )
 
 (use-package gptel
+  :disabled
   :config
   ;; Llama.cpp offers an OpenAI compatible API
 (gptel-make-openai "llama-cpp"          ;Any name
   :stream t                             ;Stream responses
   :protocol "http"
-  :host "localhost:8000"                ;Llama.cpp server location
+  :host "localhost:1234"                ;Llama.cpp server location
   :models '(test))                    ;Any names, doesn't matter for Llama)
 )
 
