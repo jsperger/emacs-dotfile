125838d0592ab67876ee6d7cb02905efd6ef8a79
John Sperger <j.sperger@gmail.com>
2025-02-07 19:06:52 -0500

    Customize: Remove underlining from flymake error, note, warning

--- customs.el
+++ customs.el
@@ -171,7 +171,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flymake-error ((t (:weight bold))))
+ '(flymake-note ((t nil)))
+ '(flymake-warning ((t (:weight bold)))))
 
 (when IS-MAC (toggle-frame-fullscreen))
 
