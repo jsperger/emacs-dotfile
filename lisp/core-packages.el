57370f5e32c84394c0de0054db97fe906f9f7ef0
John Sperger <j.sperger@gmail.com>
2025-02-07 20:14:36 -0500

    Refactor: setopt over setq

--- lisp/core-packages.el
+++ lisp/core-packages.el
@@ -38,20 +38,20 @@
   (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
   ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
+  (setopt minibuffer-prompt-properties
         '(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
   ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
   ;; Vertico commands are hidden in normal buffers.
-  (setq read-extended-command-predicate
+  (setopt read-extended-command-predicate
         #'command-completion-default-include-p)
 
   ;; Emacs 30:
   (setopt text-mode-ispell-word-completion nil)
 
   ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t)
+  (setopt enable-recursive-minibuffers t)
   )
 
 (use-package unfill)
