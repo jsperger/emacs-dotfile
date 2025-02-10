cf3c74e7b340018d1fd22f83a657a8a880335968
John Sperger <j.sperger@gmail.com>
2025-02-10 18:08:37 -0500

    Customize: remove underlining from flymake faces

--- customs.el
+++ customs.el
@@ -134,9 +134,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flymake-error ((t (:weight bold))))
- '(flymake-note ((t nil)))
- '(flymake-warning ((t (:weight bold)))))
+ '(flymake-error ((t (:underline nil :inherit default))))
+ '(flymake-note ((t (:underline nil :inherit default))))
+ '(flymake-warning ((t (:underline nil :inherit default)))))
 
 (when IS-MAC (toggle-frame-fullscreen))
 
