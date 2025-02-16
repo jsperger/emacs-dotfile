71756c356284099fbdc00150b68cf06b7d880cd2
John Sperger <j.sperger@gmail.com>
2025-02-15 22:08:00 -0500

    Config: replace all-the-icons with nerd-icons. Nerd-icons uses font glyphs instead of images (contra all-the-icons?)

--- lisp/editor-ui.el
+++ lisp/editor-ui.el
@@ -193,18 +193,65 @@
 ;; Decoration (not including fonts or themes)
 ;;;
 
+(use-package nerd-icons-dired
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package nerd-icons-completion
+  :after marginalia
+  :config
+  (setopt nerd-icons-completion-mode t)
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-ibuffer
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
+	:config
+	(setopt
+	 nerd-icons-ibuffer-icon t ; Whether display the icons.
+	 nerd-icons-ibuffer-color-icon t ; Whether display the colorful icons.
+	 ;; It respects `nerd-icons-color-icons'.
+	 nerd-icons-ibuffer-icon-size 1.0
+	 ;; The default icon size in ibuffer.
+	 nerd-icons-ibuffer-human-readable-size t ; Use human readable file size in ibuffer.
+	 ;; A list of ways to display buffer lines with `nerd-icons'.
+	 ;; See `ibuffer-formats' for details.
+	 ;; nerd-icons-ibuffer-formats
+
+	 ;; Slow Rendering
+	 ;; If you experience a slow down in performance when rendering multiple icons simultaneously,
+	 ;; you can try setting the following variable
+	 inhibit-compacting-font-caches t)
+	)
+
+(use-package nerd-icons-corfu
+	:init
+	(add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)
+;; ;; Optionally:
+;; (setq nerd-icons-corfu-mapping
+;;       '((array :style "cod" :icon "symbol_array" :face font-lock-type-face)
+;;         (boolean :style "cod" :icon "symbol_boolean" :face font-lock-builtin-face)
+;;         ;; ...
+;;         (t :style "cod" :icon "code" :face font-lock-warning-face)))
+;;         ;; Remember to add an entry for `t', the library uses that as default.
+;; The Custom interface is also supported for tuning the variable above.
+	)
+
 (use-package all-the-icons
+	:disabled
   :if (display-graphic-p))
 
 (use-package all-the-icons-dired
+	:disabled
   :after all-the-icons
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (use-package all-the-icons-ibuffer
+	:disabled
   :after all-the-icons
   :hook (ibuffer-mode . all-the-icons-ibuffer-mode))
 
 (use-package kind-icon
+	:disabled
   :after corfu
   :custom
    (kind-icon-blend-background t)
