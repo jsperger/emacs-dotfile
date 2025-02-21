b58607fd16150686557677a53b6c26cf72a67f62
John Sperger <j.sperger@gmail.com>
2025-02-20 21:40:52 -0500

    WIP ellama config. Change gptel default temp, need to check if that works with llama-cpp server

--- lisp/lang-llm.el
+++ lisp/lang-llm.el
@@ -8,32 +8,30 @@
 
 ;;; Code:
 
-
 (use-package llm
-	;; developer-facing package that gptel and others rely on
-	;; TODO: figure out how to declare providers here so they are available
-	;; to ellama, gptel, or any other front ends
+	:config
+	(use-package llm-openai
+		:ensure nil)
 	)
 
 (use-package ellama
-  :disabled
-  :init
-  (require 'llm-openai-compatible)
-   (setopt ellama-provider
-  	       (make-llm-openai-comptaible
-  :url "http://127.0.0.1:1234"))
-  :config
-  (setopt ellama-language "English")
-  )
+	:disabled
+  :after llm
+	:config
+	(setopt ellama-provider (make-llm-openai-compatible
+													 :url "http://127.0.0.1:1234"))
+	)
+
 
 (use-package gptel
   :config
+	(setopt gptel-temperature 0.8)
   ;; Llama.cpp offers an OpenAI compatible API
 (gptel-make-openai "llama-cpp"          ;Any name
   :stream t                             ;Stream responses
   :protocol "http"
   :host "localhost:1234"                ;Llama.cpp server location
-  :models '(test))                    ;Any names, doesn't matter for Llama)
+  :models '(llama-cpp))                    ;Any names, doesn't matter for Llama)
 )
 
 (provide 'lang-llm)
