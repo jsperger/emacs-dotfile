a25aef78ed9c9555c6b3abf09aa2546331cc700c
John Sperger <j.sperger@gmail.com>
2025-02-22 20:42:18 -0500

    Enable tabspaces with tab-bar-mode. Don't restrict buffers by space

--- lisp/editor-projects.el
+++ lisp/editor-projects.el
@@ -83,10 +83,11 @@
   :config (projection-multi-embark-setup-command-map))
 
 (use-package tabspaces
-  :disabled
-  :hook (prog-mode . tabspaces-mode)
+  :hook (tab-bar-mode . tabspaces-mode) 
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
   :custom
-  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-use-filtered-buffers-as-default nil)
   (tabspaces-default-tab "Default")
   (tabspaces-remove-to-default t)
   (tabspaces-include-buffers '("*scratch*"))
@@ -96,6 +97,43 @@
   (tabspaces-session t)
   (tabspaces-session-auto-restore t)
   (tab-bar-new-tab-choice "*scratch*")
+
+
+	:config
+;; Filter Buffers for Consult-Buffer
+
+(with-eval-after-load 'consult
+;; hide full buffer list (still available with "b" prefix)
+(consult-customize consult--source-buffer :hidden t :default nil)
+;; set consult-workspace buffer list
+(defvar consult--source-workspace
+  (list :name     "Workspace Buffers"
+        :narrow   ?w
+        :history  'buffer-name-history
+        :category 'buffer
+        :state    #'consult--buffer-state
+        :default  t
+        :items    (lambda () (consult--buffer-query
+                         :predicate #'tabspaces--local-buffer-p
+                         :sort 'visibility
+                         :as #'buffer-name)))
+
+  "Set workspace buffer list for consult-buffer.")
+(add-to-list 'consult-buffer-sources 'consult--source-workspace))
+
+(defun my--consult-tabspaces ()
+  "Deactivate isolated buffers when not using tabspaces."
+  (require 'consult)
+  (cond (tabspaces-mode
+         ;; hide full buffer list (still available with "b")
+         (consult-customize consult--source-buffer :hidden t :default nil)
+         (add-to-list 'consult-buffer-sources 'consult--source-workspace))
+        (t
+         ;; reset consult-buffer to show all buffers 
+         (consult-customize consult--source-buffer :hidden nil :default t)
+         (setq consult-buffer-sources (remove #'consult--source-workspace consult-buffer-sources)))))
+
+(add-hook 'tabspaces-mode-hook #'my--consult-tabspaces)
 
 	;; (defvar tabspaces-command-map
 	;;   (let ((map (make-sparse-keymap)))
