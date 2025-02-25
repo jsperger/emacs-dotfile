41f6f287ca62b85ce2d1b7496e86df567d6a2c2b
John Sperger <j.sperger@gmail.com>
2025-02-24 19:05:18 -0500

    docs: Add more packages to the mountain 'to-try' pile

--- lisp/editor-ui.el
+++ lisp/editor-ui.el
@@ -151,6 +151,28 @@
 (use-package golden-ratio
   :config (golden-ratio-mode nil))
 
+(use-package eyebrowse
+	:disabled)
+
+(use-package activities
+	:disabled
+  :init
+  (activities-mode)
+  (activities-tabs-mode)
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+
+  :bind
+  (("C-x C-a C-n" . activities-new)
+   ("C-x C-a C-d" . activities-define)
+   ("C-x C-a C-a" . activities-resume)
+   ("C-x C-a C-s" . activities-suspend)
+   ("C-x C-a C-k" . activities-kill)
+   ("C-x C-a RET" . activities-switch)
+   ("C-x C-a b" . activities-switch-buffer)
+   ("C-x C-a g" . activities-revert)
+   ("C-x C-a l" . activities-list)))
+
 
 (use-package popper
   :config
