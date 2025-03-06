dc543c5ce11ffc7f05f570ab901c7f342ae4bbe5
John Sperger <j.sperger@gmail.com>
2025-03-05 22:40:06 -0500

    chore: add web (hugo) packages to test

--- lisp/lang-web.el
+++ lisp/lang-web.el
@@ -8,11 +8,26 @@
 
 ;;; Code:
 
+(use-package ox-hugo
+	:disabled)
+
 (use-package easy-hugo
+	:disabled
   :config
   (setq
    easy-hugo-basedir "~/work/code/websites/jsperger/"
    ))
+
+(use-package verb
+	:disabled
+	;; org-mode and http requests
+
+	;; structure specifications for HTTP requests using Org's tree structure.
+	;; Properties defined in the child headings extend or override properties
+	;; defined in the parent headings - this way, it is possible to author HTTP
+	;; requests without having to repeat things such as URL hosts, headers, ports,
+	;; etc.
+	)
 
 (use-package mermaid-mode
 			:mode ("\\.mermaid\\'" . mermaid-mode))
