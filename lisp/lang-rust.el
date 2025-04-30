8e0ce56f18cdd2182a24c93341167a70452fb0de
John Sperger <j.sperger@gmail.com>
2025-04-30 04:53:47 -0400

    🏗️: update rust mode and keybinds

--- lisp/lang-rust.el
+++ lisp/lang-rust.el
@@ -9,21 +9,33 @@
 ;;; Code:
 
 (use-package rust-mode
-	:mode ("\\.rs\\'" . rust-ts-mode)
-;;  :hook (rust-mode . 'eglot-ensure)
-  :config
-    ;; (add-to-list 'eglot-server-programs
-    ;;          '((rust-ts-mode rust-mode) .
-    ;;            ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
-
-  (despot-def (Rust-mode-map)
+	:mode ("\\.rs\\'" . rust-mode)
+  :general
+  (despot-def (rust-mode-map)
       :major-modes '(rust-ts-mode rust-mode)
-      "c"            'rust-compile)
+      "c"            'rust-compile
+			"k" 'rust-check
+			"t" 'rust-test
+			"r" 'rust-run
+			"F" 'rust-format-buffer)
+	)
 
-)
-
-
-
+(use-package cargo-mode
+  :hook
+  (rust-mode . cargo-minor-mode)
+  :config
+  (setopt compilation-scroll-output t
+					rust-format-on-save t)
+	:general
+	(despot-def (rust-mode-map)
+		:major-modes '(rust-mode rust-ts-mode)
+		"e" 'cargo-mode-execute-task
+		"t" 'cargo-mode-test
+		"l" 'cargo-mode-last-command
+		"b" 'cargo-mode-build
+		"o" 'cargo-mode-test-current-buffer
+		"f" 'cargo-mode-test-current-test)
+	)
 (provide 'lang-rust)
 ;; Local Variables:
 ;; no-byte-compile: t
