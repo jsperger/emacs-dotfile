7596c7ae8b801c0a8b66a30b307a6c5c56f15d49
John Sperger <j.sperger@gmail.com>
2025-07-15 04:22:56 -0400

    refactor: Move tools-org-capture.el to config/setup-org-capture.el

---
+++ config/setup-org-capture.el
@@ -0,0 +1,11 @@
+;;; config/setup-org-capture.el --- Org capture tools configuration -*- lexical-binding: t -*-
+
+(use-package doct)
+
+;;; setup-org-capture.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
