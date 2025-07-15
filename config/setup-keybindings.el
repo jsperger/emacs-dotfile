9830b65a69cdf88c22337c2d5495abfc089c09c4
John Sperger <j.sperger@gmail.com>
2025-07-15 04:16:24 -0400

    refactor: Split core-keybinds.el into setup and helpers

---
+++ config/setup-keybindings.el
@@ -0,0 +1,27 @@
+;;; config/setup-keybindings.el --- Keybindings configuration -*- lexical-binding: t -*-
+
+(require 'my-keybindings)
+
+(use-package which-key
+	:hook (elpaca-after-init)
+  :config
+  (setq which-key-idle-delay 0.4
+        which-key-idle-secondary-delay 0.01
+        which-key-max-description-length 32
+        which-key-sort-order 'which-key-key-order-alpha
+        which-key-allow-evil-operators t)
+  (push '((nil . "tab-bar-select-tab") . t) which-key-replacement-alist))
+
+(use-package general
+	:ensure (:wait t)
+	:demand t
+  :config
+  (setq general-emit-autoloads nil))
+
+;;; setup-keybindings.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
