ebe45a6242f999bf050bb0fd7d16b781d69c9bcf
John Sperger <j.sperger@gmail.com>
2025-07-15 04:19:39 -0400

    refactor: Move lang-lisp.el to config/setup-lisp.el

---
+++ config/setup-lisp.el
@@ -0,0 +1,45 @@
+;;; config/setup-lisp.el --- Lisp configuration -*- lexical-binding: t -*-
+
+(use-package slime
+	:hook (elisp-mode . slime-mode)
+  :config
+	(setopt inferior-lisp-program "sbcl")
+	(add-hook 'slime-mode-hook (setq-local lsp-bridge-mode -1))
+  (despot-def (slime-mode-map)
+    :major-modes 'slime-mode
+    "b" 'slime-eval-buffer
+    "c" 'slime-compile-defun
+    "k" 'slime-compile-and-load-file
+    "f"  'ad-Advice-slime-eval-defun
+    "r" 'slime-eval-region
+    )
+  )
+
+(use-package elisp-mode
+  :ensure nil
+  :config
+  (despot-def :keymaps '(emacs-lisp-mode-map lisp-interaction-mode-map)
+    "'"  'ielm
+    "c"  (cons "compile" (make-sparse-keymap))
+    "cc" 'emacs-lisp-byte-compile
+    "e"  (cons "eval" (make-sparse-keymap))
+    "eb" 'eval-buffer
+    "ee" 'eval-last-sexp
+    "er" 'eval-region
+    "ef" 'eval-defun
+    "t"  (cons "tests" (make-sparse-keymap))
+    "tb" 'ert-run-tests-buffer
+    "tq" 'ert))
+
+(use-package racket-mode
+	:mode ("\\.rkt\\" . racket-mode)
+	)
+
+;;; setup-lisp.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
+
