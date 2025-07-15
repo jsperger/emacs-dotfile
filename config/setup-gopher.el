9fb3bc4b5299ff082943655d3da938b6b2b1bd02
John Sperger <j.sperger@gmail.com>
2025-07-15 04:23:25 -0400

    refactor: Move tools-web.el to config/setup-gopher.el

---
+++ config/setup-gopher.el
@@ -0,0 +1,13 @@
+;;; config/setup-gopher.el --- Gopher configuration -*- lexical-binding: t -*-
+
+(use-package elpher
+	:general
+	(despot-def "ag" 'elpher))
+
+;;; setup-gopher.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
