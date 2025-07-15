e914b74c32fa3aaef4d4cb200a1c03e8b73e24ce
John Sperger <j.sperger@gmail.com>
2025-07-15 04:23:02 -0400

    refactor: Move tools-programming.el to config/setup-programming.el

---
+++ config/setup-programming.el
@@ -0,0 +1,40 @@
+;;; config/setup-programming.el --- General programming tools configuration -*- lexical-binding: t -*-
+
+(use-package apheleia
+  :disabled
+  :custom (apheleia-global-mode nil)
+  )
+
+(use-package editorconfig
+	:disabled 
+  :config
+  (editorconfig-mode nil)
+	:general
+	(tyrant-def "ce" 'editorconfig-mode)
+	)
+
+(use-package typespec-ts-mode
+	:mode ("\\.tsp\\'" . typespec-ts-mode)
+	:config
+	(add-to-list
+ 'treesit-language-source-alist
+ '(typespec "https://github.com/happenslol/tree-sitter-typespec"))
+	)
+
+(use-package code-cells
+	:disabled
+	:mode ("\\([ipynb]\\)\\'" . code-cells-mode))
+
+(use-package drepl
+	:disabled
+	:general
+	(trant-def "ci" 'drepl-ipython))
+
+;;; setup-programming.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
+
