a8ecbe6dc76448f6e381bb6291820c85a693acc6
John Sperger <j.sperger@gmail.com>
2025-07-15 04:21:31 -0400

    refactor: Move tools-diagramming.el to config/setup-diagramming.el

---
+++ config/setup-diagramming.el
@@ -0,0 +1,45 @@
+;;; config/setup-diagramming.el --- Diagramming tools configuration -*- lexical-binding: t -*-
+
+(use-package chatu
+  :hook ((org-mode markdown-mode) . chatu-mode)
+  :commands (chatu-add
+             chatu-open)
+  :custom ((chatu-input-dir "./draws")
+           (chatu-output-dir "./draws_out")))
+
+(use-package d2-mode
+	:mode ("\\.d2\\'")
+	:general
+	(despot-def (d2-mode-map)
+		:major-modes '(d2-mode)
+		"c" 'd2-compile
+		"f" 'd2-compile-file
+		"b" 'd2-compile-buffer
+		"r" 'd2-compile-region
+		"h" 'd2-compile-file-and-browse
+		"j" 'd2-compile-buffer-and-browse
+		"k" 'd2-compile-region-and-browse
+		"o" 'd2-open-browser
+		"o" 'd2-view-current-svg
+		"d" 'd2-open-doc)
+	)
+
+(use-package mermaid-mode
+			:mode ("\\.mermaid\\''" . mermaid-mode))
+
+(use-package pikchr-mode
+		:mode ("\\.pikchr\\''" . pikchr-mode))
+
+(use-package uniline
+	:general
+(tyrant-def "au" 'uniline-mode)
+	)
+
+;;; setup-diagramming.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
+
