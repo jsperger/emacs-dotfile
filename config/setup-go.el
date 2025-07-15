902878697b2ca85609ed7a2277e09018b630e20f
John Sperger <j.sperger@gmail.com>
2025-07-15 04:19:29 -0400

    refactor: Move lang-go.el to config/setup-go.el

---
+++ config/setup-go.el
@@ -0,0 +1,25 @@
+;;; config/setup-go.el --- Go programming language configuration -*- lexical-binding: t -*-
+
+(use-package go-mode
+  :mode ("\.go\"" . go-mode)
+  :general
+  (despot-def (go-mode-map)
+    :major-modes '(go-mode go-ts-mode)
+    "a" 'gofmt
+    "f" (cons "jump" (make-sparse-keymap))
+    "fa" 'go-goto-arguments
+    "ff" 'go-goto-function
+    "fn" 'go-goto-function-name
+    "fr" 'go-goto-return-values
+    "fm" 'go-goto-method-receiver
+    )
+	)
+
+;;; setup-go.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
+
