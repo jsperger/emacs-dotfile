d4ea8c220e3218fb040e3d7d5410e883ab451fc0
John Sperger <j.sperger@gmail.com>
2025-07-15 04:18:09 -0400

    refactor: Split editor-themes.el into setup and helpers

---
+++ config/setup-themes.el
@@ -0,0 +1,52 @@
+;;; config/setup-themes.el --- Themes configuration -*- lexical-binding: t -*-
+
+(require 'my-theme-helpers)
+
+(use-package catppuccin-theme
+  :defer t)
+
+(use-package doom-themes)
+
+(use-package doric-themes)
+
+(use-package ef-themes
+  :config
+  (setopt ef-themes-mixed-fonts t)
+  )
+
+(use-package flexoki-themes
+	:disabled
+  :custom
+  (flexoki-themes-use-bold-keywords t)
+  (flexoki-themes-use-bold-builtins t)
+  (flexoki-themes-use-italic-comments nil))
+
+(use-package evenok
+	:disabled
+	)
+
+(use-package gruvbox-theme)
+
+(use-package kaolin-themes)
+
+(use-package nordic-night-theme)
+
+(use-package modus-themes)
+
+(use-package solarized-theme)
+
+(advice-add 'load-theme :after #'load-theme@run-hooks)
+(advice-add 'load-theme :before #'load-theme@theme-dont-propagate)
+
+(add-hook 'after-load-theme-hook #'bolder-faces)
+(add-hook 'after-load-theme-hook #'customize-tab-bar)
+(add-hook 'after-load-theme-hook #'unscale-outlines)
+(add-hook 'after-load-theme-hook #'my-disable-flymake-underline)
+
+;;; setup-themes.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
---
+++ lisp/my-theme-helpers.el
@@ -0,0 +1,50 @@
+;;; lisp/my-theme-helpers.el --- Helper functions for themes -*- lexical-binding: t -*-
+
+(defvar after-load-theme-hook nil
+  "Hook run after a color theme is loaded using `load-theme'.")
+
+(defun load-theme@run-hooks (&rest _)
+  "Run `after-load-theme-hook'."
+  (run-hooks 'after-load-theme-hook))
+
+(defun load-theme@theme-dont-propagate (&rest _)
+  "Discard all themes before loading new."
+  (mapc #'disable-theme custom-enabled-themes))
+
+(defun bolder-faces ()
+  (set-face-attribute 'font-lock-function-name-face nil :weight 'semi-bold)
+  (set-face-attribute 'font-lock-keyword-face nil :weight 'semi-bold))
+
+(defun customize-tab-bar ()
+  "Customize tab-bar faces."
+  (set-face-attribute 'tab-bar nil
+                      :foreground 'unspecified
+                      :background 'unspecified
+                      :box `(:line-width (-1 . 4) :color ,(face-background 'default))
+                      :inherit 'unspecified)
+  (set-face-attribute 'tab-bar-tab nil
+                      :weight 'bold
+                      :box 'unspecified
+                      :foreground 'unspecified
+                      :background 'unspecified
+                      :inherit 'unspecified)
+  (set-face-attribute 'tab-bar-tab-inactive nil
+                      :box 'unspecified
+                      :foreground 'unspecified
+                      :background 'unspecified
+                      :inherit 'unspecified))
+
+(defun unscale-outlines ()
+  (dolist (outline-number (number-sequence 1 8))
+    (let ((outline (intern (format "outline-%d" outline-number))))
+      (set-face-attribute outline nil :height 1.0))))
+
+(defun my-disable-flymake-underline ()
+  "Disable underlining for Flymake faces while keeping theme colors."
+  (custom-set-faces
+   `(flymake-error ((t (:underline nil :inherit default))))
+   `(flymake-note ((t (:underline nil :inherit default))))
+   `(flymake-warning ((t (:underline nil :inherit default))))))
+
+(provide 'my-theme-helpers)
+;;; my-theme-helpers.el ends here
