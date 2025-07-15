cd3d52ddeac273c052bed30172f31834cfa802ac
John Sperger <j.sperger@gmail.com>
2025-07-15 04:21:20 -0400

    refactor: Move tools-casual.el to config/setup-casual.el

---
+++ config/setup-casual.el
@@ -0,0 +1,12 @@
+;;; config/setup-casual.el --- Casual suite configuration -*- lexical-binding: t -*-
+
+(use-package casual
+	:disabled)
+
+;;; setup-casual.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
