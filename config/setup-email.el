d3286624be14e46f73fa0eb6c74c66e537541020
John Sperger <j.sperger@gmail.com>
2025-07-15 04:21:48 -0400

    refactor: Move tools-email.el to config/setup-email.el

---
+++ config/setup-email.el
@@ -0,0 +1,15 @@
+;;; config/setup-email.el --- Email configuration -*- lexical-binding: t -*-
+
+(use-package mu4e
+  :disabled)
+
+(use-package notmuch
+  :disabled)
+
+;;; setup-email.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
