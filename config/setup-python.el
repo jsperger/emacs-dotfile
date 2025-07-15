dbcaa086168686cba7bd14dcf06970e290e98a68
John Sperger <j.sperger@gmail.com>
2025-07-15 04:20:18 -0400

    refactor: Move lang-python.el to config/setup-python.el

---
+++ config/setup-python.el
@@ -0,0 +1,26 @@
+;;; config/setup-python.el --- Python configuration -*- lexical-binding: t -*-
+
+(use-package python-x
+	)
+
+(use-package live-py-mode
+	:general
+	  (despot-def (python-mode-map)
+    :major-modes '(python-mode python-ts-mode)
+    "l" 'live-py-mode
+    )
+	)
+
+(use-package uv-mode
+	:hook (python-mode . uv-mode))
+
+(use-package pyenv-mode
+	:disabled)
+
+;;; setup-python.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
