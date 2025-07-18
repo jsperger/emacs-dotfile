e845c27f21d7eab1782ded07646b15d754ec53cb
John Sperger <j.sperger@gmail.com>
2025-07-18 02:36:35 -0400

    fix: outlining tangle destination

--- config/setup-outlining.el
+++ config/setup-outlining.el
@@ -1,19 +1,23 @@
 ;;; setup-outlining.el ---  -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
+(use-package outline-minor-faces
+  :after outline
+  :config (add-hook 'outline-minor-mode-hook
+                    #'outline-minor-faces-mode)
+  )
 
 (use-package backline
   :after outline
-  :config (advice-add 'outline-flag-region :after 'backline-update))
+  :config (advice-add 'outline-flag-region :after 'backline-update)
+  )
 
 (use-package outline-indent
-	:commands outline-indent-minor-mode
-	:custom
+  :commands outline-indent-minor-mode
+  :custom
   (outline-indent-ellipsis " ▼ ")
   :general
   (tyrant-def
     "to" 'outline-indent-minor-mode
-    "o"       (cons "outline" (make-sparse-keymap))
+    "o"  (cons "outline" (make-sparse-keymap))
     "oa" '("all open" . outline-indent-open-folds)
     "oA" '("all closed" . outline-indent-close-folds)
     "ob" 'bicycle-cycle
@@ -21,20 +25,13 @@
     "oc" 'outline-indent-close-fold
     "oj" 'outline-forward-same-level
     "ok" 'outline-backward-same-level
-    "oh"      'hs-minor-mode
+    "oh" 'hs-minor-mode
     "oo" 'outline-indent-open-fold
-    "oO"      'outline-minor-mode
+    "oO" 'outline-minor-mode
     "or" 'outline-indent-open-fold-rec
     "ot" 'bicycle-cycle
     "oT" 'bicycle-cycle-global
-    "oz"      'TeX-fold-mode
-    )
-  )
-
-(use-package outline-minor-faces
-  :after outline
-  :config (add-hook 'outline-minor-mode-hook
-                    #'outline-minor-faces-mode)
+    "oz" 'TeX-fold-mode)
   )
 
 ;; Local Variables:
