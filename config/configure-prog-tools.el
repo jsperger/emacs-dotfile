0f1fe4853112585cdd623098cf39f2c7c4422f0c
John Sperger <j.sperger@gmail.com>
2025-07-31 17:36:26 -0400

    🏗️: start consolidating programming tools not specific to one lang

---
+++ config/configure-prog-tools.el
@@ -0,0 +1,41 @@
+;;; configure-prog-tools.el --- programming add-ons -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;;;; ========================= Language Server packages ========================
+
+;; [[file:../its-lit.org::lsp-bridge block][lsp-bridge block]]
+(use-package lsp-bridge
+  :after yasnippet
+
+  :ensure (lsp-bridge :type git
+                      :host github
+                      :repo "manateelazycat/lsp-bridge"
+                      :files (:defaults "*.el" "*.py" "acm" "core" "langserver"
+                                        "multiserver" "resources")
+                      :build (:not elpaca--byte-compile)
+                      )
+
+  :config  (setopt toml-indent-offset 2
+                   lsp-bridge-enable-hover-diagnostic t
+                   lsp-bridge-enable-org-babel t
+                   acm-enable-preview t
+                   )
+
+  :general  (tyrant-def "l" (cons "lsp" (make-sparse-keymap))
+              "lf" 'lsp-bridge-find-def
+              "lF" 'lsp-bridge-find-def-other-window
+              "lh" 'lsp-bridge-show-documentation
+              "lp" 'lsp-bridge-popup-documentation
+              )
+  )
+
+(add-hook 'elpaca-after-init-hook #'global-lsp-bridge-mode)
+;; lsp-bridge block ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; configure-prog-tools.el ends here
